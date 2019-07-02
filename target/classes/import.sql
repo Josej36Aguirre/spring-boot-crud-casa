d793c273b75ecc1b7088d7676b980be53ecdf99d
Josej36Aguirre <josej36.aguirre@gmail.com>
2019-07-02 05:52:40 -0500

    Initial commit

---
+++ target/classes/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(id, nombre, apellido, email, cerate_at) VALUES(1, 'JOSE', 'AGUIRRE', 'josej36.aguirre@gmail.com','2017-08-28'); 
+INSERT INTO clientes(id, nombre, apellido, email, cerate_at) VALUES(2, 'ALFONSO', 'AGUIRRE', 'aguirrearias2003@gmail.com','2017-08-29');
